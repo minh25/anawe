149635f63edca3e62d138c7a95dc2bb0c18adf4b
maxxam <minh00012258@gmail.com>
2020-07-14 15:20:47 +0700

    add try/catch

--- soup_of_url.py
+++ soup_of_url.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-def soup_of_url(url_name, time_out=10):
+def soup_of_url(url_name, time_out=20):
     '''
     :return: soup of page
     '''
-    data = requests.get(url_name, timeout=time_out)
-    soup = BeautifulSoup(data.text, features='html.parser')
-    return soup
+    try:
+        data = requests.get(url_name, timeout=time_out)
+        soup = BeautifulSoup(data.text, features='html.parser')
+        return soup
+    except Exception as error:
+        print(url_name, error)
