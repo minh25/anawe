5502f589153cd0a7739cc1239bb8286bb64117fa
maxxam <minh00012258@gmail.com>
2020-07-22 11:58:13 +0700

    clean

--- analyze_a_product.py
+++ analyze_a_product.py
@@ -1,4 +1,9 @@
-from soup_of_url import soup_of_url
+import soup_of_url
+
+import queue
+import threading
+
+detail_products = queue.Queue()
 
 
 def extract_product_price(soup, do_print=True, url='#'):
@@ -68,19 +73,55 @@ def extract_campaign_details(soup, do_print=True, url='#'):
     return campaign_details
 
 
-def analyze_soup_of_a_product(soup, urlx):
-    detail = [extract_product_price(soup=soup, do_print=False, url=urlx),
-              extract_image_urls(soup=soup, do_print=False, url=urlx),
-              extract_product_name(soup=soup, do_print=False, url=urlx),
-              extract_campaign_details(soup=soup, do_print=False, url=urlx)]
+def analyze_soup_of_a_product(soup, url):
+    detail = [extract_product_price(soup=soup, do_print=False, url=url),
+              extract_image_urls(soup=soup, do_print=False, url=url),
+              extract_product_name(soup=soup, do_print=False, url=url),
+              extract_campaign_details(soup=soup, do_print=False, url=url)]
 
     return detail
 
 
-def main(url):
-    for x in analyze_soup_of_a_product(soup_of_url(url), url):
-        print(x)
+def get_detail_of_product(product_urls=queue.Queue()):
+    while True:
+        try:
+            url = product_urls.get()
+            while True:
+                soup = soup_of_url.soup_of_url(url)
+                details = analyze_soup_of_a_product(soup, url)
+                if details[0] == '#':
+                    continue
+                break
+
+            product_code = url[-39:]
+            url_obj = {'url': url, 'product_code': product_code,
+                       'product_price': details[0],
+                       'image_urls': details[1],
+                       'product_name': details[2],
+                       'campaign_details': details[3]}
+
+            detail_products.put(url_obj)
+        finally:
+            product_urls.task_done()
+
+
+def main():
+    q = queue.Queue()
+
+    for _ in range(30):
+        q.put(
+            "https://www.giftza.co/campaigns/department/housewares/product/mug/tags/Holidays%20%26%20Events/mom-pee"
+            "-yourself?retailProductCode=DBD375D877A0B6-2A4918C9F9EE-MS0-TC1001-WHT")
+
+    for _ in range(30):
+        threading.Thread(target=get_detail_of_product, daemon=True, args=(q,)).start()
+
+    q.join()
 
 
 if __name__ == '__main__':
-    main('https://www.giftza.co/campaigns/-/-/tags/Holidays%20%26%20Events/stop-staring-at-my-shamrocks-1?retailProductCode=DBD375D877A0B6-9A280E99A8AE-GS2-TC2-KGR')
+    main()
+
+    while detail_products.qsize() > 0:
+        x = detail_products.get()
+        print(x)
